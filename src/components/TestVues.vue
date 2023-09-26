9867af7eff1bfe0ffe7168300735430de8d9f013
zjoker120 <zjoker120@gmail.com>
2023-09-26 16:26:17 +0900

    체크박스 테스트

--- src/components/TestVues.vue
+++ src/components/TestVues.vue
@@ -6,11 +6,22 @@
         </div>
     </div>
     <div>
-        
+        <input type="checkbox" value="사과" v-model="fu"/><label>사과</label>
+        <input type="checkbox" value="바나나" v-model="fu"/><label>바나나</label>
+        <input type="checkbox" value="자두" v-model="fu"/><label>자두</label>
+        <button @click="pickFruits">pick</button>
+        <p>선택한 과일 : {{selectedFruit}}</p>
     </div>
 </template>
 
 <script setup>
+    import { ref } from 'vue';
+    const fu = ref([]);
+    const selectedFruit = ref([]);
+
+    const pickFruits = () => {
+        selectedFruit.value = fu.value.filter(fruit => fu.value.includes(fruit));
+    };
 </script>
 
 <style scoped>
