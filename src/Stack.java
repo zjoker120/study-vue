aaec18b4e2b7049e0a1d68ac53a7e1b1615930b7
zjoker120 <zjoker120@gmail.com>
2023-12-06 16:04:42 +0900

    feat: stack - java로

--- src/Stack.java
+++ src/Stack.java
@@ -1,19 +1,62 @@
-package src;
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.List;
 
 public class Stack {
-    void push(Object item){
+    private List<Object> stackList;
 
+    public Stack() {
+        this.stackList = new ArrayList<>();
     }
-    void pop(){
 
+    public void push(Object item) {
+        stackList.add(item);
     }
-    void peek(){
 
+    public void pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        stackList.remove(stackList.size() - 1);
     }
-    void isEmpty(){
 
+    public void peek() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        Object top = stackList.get(stackList.size() - 1);
+        System.out.println("Top element: " + top);
     }
-    void printStack(){
-        
+
+    public boolean isEmpty() {
+        return stackList.isEmpty();
+    }
+
+    public void printStack() {
+        if (isEmpty()) {
+            System.out.println("Stack is empty.");
+        } else {
+            System.out.print("Stack: ");
+            for (Object item : stackList) {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack myStack = new Stack();
+
+        myStack.push(1);
+        myStack.push(2);
+        myStack.push(3);
+
+        myStack.printStack();
+
+        myStack.pop();
+
+        myStack.printStack();
+
+        myStack.peek();
     }
 }
